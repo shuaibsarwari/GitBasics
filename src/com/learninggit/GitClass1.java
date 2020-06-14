4ca57433adb94631eba7dc0bcd3d165ead2ff58b
shuaibsarwari <shuaibsarwari@gmail.com>
2020-06-14 09:03:07 -0400

    commiting changes to test 2

---
+++ src/com/learninggit/GitClass1.java
@@ -0,0 +1,20 @@
+package com.learninggit;
+
+public class GitClass1 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("Hello hello world");
+		boolean work= true;
+		boolean home= false;
+		
+		System.out.println("i will be at"+ work);
+		System.out.println("i will be"+ home);
+		
+		System.out.println("love is life");
+
+	}
+
+}
